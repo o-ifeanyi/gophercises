8494a107fc478a075c09109c57b081b7226bb9f2
o-ifeanyi <onuifeanyi95@gmail.com>
2022-07-26 16:19:06 +0100

    Update name

--- go-cli-task-mgr/cmd/root.go
+++ go-cli-task-mgr/cmd/root.go
@@ -4,5 +4,5 @@ import "github.com/spf13/cobra"
 
 var TaskBucket = []byte("tasks")
 var CompleteBucket = []byte("complete")
-var RootCmd = &cobra.Command{Use: "task",
+var RootCmd = &cobra.Command{Use: "go-cli-task-mgr",
 	Short: "Task is a CLI task manager"}
